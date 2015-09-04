cbb0fb43509ef44ddcab660c7f1bad73e316a1fc
wayfr <wf@skilz.ru>
2015-09-05 02:59:41 +0300

    main module chk

--- 00main.py
+++ 00main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
-try:
-	import prlsdkapi
-except:
-	print 'Failed to import Parallels SDK'
-	
+#try:
+#	import prlsdkapi
+#except:
+#	print 'Failed to import Parallels SDK'
 
-import logging, time, commands
+
+import logging, time, commands,hashlib
 from config import *
 from uuid import uuid4
+
+spacer = lambda x: x + '\n'
+def output_w(result):
+	with open('results.log', 'a') as file:
+		file.write(spacer(result))
+
+
+
+
+
+
+
+
+def main():
+	output_w('t')
+	output_w('q')
+main()
