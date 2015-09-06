e4b1a084022dd3d4eafcccb96d1f28fba8ddfb37
wayfr <wf@skilz.ru>
2015-09-06 19:01:59 +0300

    added logging, host slicing

--- 00main.py
+++ 00main.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
+
 #try:
 #	import prlsdkapi
 #except:
 #	print 'Failed to import Parallels SDK'
 
 
-import logging, time, commands,hashlib
+import logging, time, commands, hashlib, re
 from config import *
 from uuid import uuid4
 
+#host_slicer=lambda x: re.findall(r"[\w^\.]+", x) #i know, it's ugly, host='%s:%s@%s' %(s_root, s_pass, s_host) looks better but regex more interest
+host_slicer=lambda x: re.split('[:@]',x)
 spacer = lambda x: x + '\n'
-sshckbypass='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '
 
-source_node=sshckbypass+source_node
-dest_node=sshckbypass+dest_node
+sshckbypass='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '
+#consts = prlsdkapi.prlsdk.consts
+ssh_s_node=sshckbypass+source_node
+ssh_d_node=sshckbypass+dest_node
+
+class Halt(Exception):
+	pass
+
 def output_w(result):
 	with open('results.log', 'a') as file:
 		file.write(spacer(result))
@@ -26,6 +34,11 @@ def output_w(result):
 
 
 def main():
+	logging.basicConfig(filename="checklist.log", level=logging.INFO)
+
 	output_w(source_node)
 	output_w(dest_node)
-main()
+	print host_slicer(dest_node) 
+
+if __name__ == "__main__":
+	main()
