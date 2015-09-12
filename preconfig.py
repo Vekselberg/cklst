609520c9f9b8e971dda8e3fa15921ea81f2e4aa9
Yaroslav <ykargin@parallels.com>
2015-09-12 10:22:14 +0300

    snapshots improovement

--- preconfig.py
+++ preconfig.py
@@ -2,8 +2,10 @@
 import commands
 from config import *
 try:
-	print commands.getoutput('ssh-copy-id -i dest.pub %s' %source_node.split('@')[1])
-	print 'Security key added'
+	commands.getoutput('ssh-copy-id -i dest.pub %s' %source_node.split('@')[1])
+	print 'Security key added on SUT'
+	commands.getoutput('ssh-copy-id -i dest.pub %s' %dest_node.split('@')[1])
+	print 'Security key added on slave'
 except:
 	print 'Unable to add key'
 	exit(1)
