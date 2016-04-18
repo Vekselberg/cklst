d095e449120ddc8b3651252e5e60373408a14a10
root <root@TAKEDA.qa.sw.ru>
2016-04-18 18:23:03 +0300

    preconfig improovements

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-source_node='root:1q2w3e@mccp6.qa.sw.ru' #System under test
-dest_node='' #Slave node, for VE migration checks. May be ='' if migration checks not needed.
+source_node='root:1q2w3e@takeda.qa.sw.ru' #System under test
+dest_node='root:96I8x301@52.39.9.205' #Slave node, for VE migration checks. May be ='' if migration checks not needed.
--- preconfig.py
+++ preconfig.py
@@ -7,8 +7,11 @@ try:
 	commands.getoutput('ssh-copy-id -i dest.pub %s' %source_node.split('@')[1])
 	print 'Security key added on SUT'
 	if dest_node !='':
-		commands.getoutput('ssh-copy-id -i dest.pub %s' %dest_node.split('@')[1])
-		print 'Security key added on slave'
+		slave=commands.getstatusoutput('ssh-copy-id -i dest.pub %s' %dest_node.split('@')[1])
+		if slave[0] != 0:
+			print 'Unable to add destination node key:\n', slave[1]
+		else:
+			print 'Security key added on slave.'
 	else:
 		print 'No slave node provided'
 except:
